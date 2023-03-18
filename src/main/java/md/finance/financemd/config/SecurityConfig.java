4ef25d8b80d783663d6c961ec380407905ea54e3
ArlletGod <arlletgod@gmail.com>
2023-03-18 20:57:51 +0200

    add CSRF

--- src/main/java/md/finance/financemd/config/SecurityConfig.java
+++ src/main/java/md/finance/financemd/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
+        http.authorizeRequests()
                 .antMatchers("/auth/login", "/error", "/auth/registration")
                 .permitAll()
                 .anyRequest()
