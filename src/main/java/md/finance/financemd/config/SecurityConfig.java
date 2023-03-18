40552dcd57690c5843335a123129849898953927
ArlletGod <arlletgod@gmail.com>
2023-03-18 19:26:43 +0200

    modify spring security v2

--- src/main/java/md/finance/financemd/config/SecurityConfig.java
+++ src/main/java/md/finance/financemd/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package md.finance.financemd.config;
 import md.finance.financemd.service.PersonDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -18,11 +19,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.personDetailsService = personDetailsService;
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/auth/login","/error")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/" ,true)
+                .failureForwardUrl("/auth/login?error");
+    }
 
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(personDetailsService);
 
     }
+
 
     @Bean
     public PasswordEncoder getPasswordEncoder(){
